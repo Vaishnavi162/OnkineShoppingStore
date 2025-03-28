5b22897ec195462ff46adfe98ad2f752aee0b2d4
Vaishu162 <dhorawalavaishnavi@gmail.com>
2025-03-28 16:40:08 +0530

    added

---
+++ Models/Home/HomeIndexViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Drawing.Printing;
+using System.Linq;
+using OnlineShopingStore.DAL;
+using OnlineShopingStore.Repository;
+using PagedList;
+using PagedList.Mvc;
+
+namespace OnlineShopingStore.Models.Home
+{
+    public class HomeIndexViewModel
+    {
+        public GenericUnitOfWork _unitOfWork = new GenericUnitOfWork();
+        dbMyOnlineShoppingEntities1 contex = new dbMyOnlineShoppingEntities1();
+        public IPagedList<Tbl_Product> ListofProducts { get; set; }
+
+        public HomeIndexViewModel CreateModel(string search, int pageSize, int? page)
+        {
+            SqlParameter[] param = new SqlParameter[]
+            {
+                new SqlParameter("@search",search ?? (object)DBNull.Value)
+            };
+            IPagedList<Tbl_Product> data = contex.Database.SqlQuery<Tbl_Product>("GetBySearch @search", param).ToList().ToPagedList(page ?? 1, pageSize);
+            return new HomeIndexViewModel
+            {
+                ListofProducts = data
+            };
+        }
+
+
+    }
+}
---
+++ Models/Home/Item.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using OnlineShopingStore.DAL;
+
+namespace OnlineShopingStore.Models.Home
+{
+    public class Item
+    {
+        public Tbl_Product Product { get; set; }
+        public int Quantity { get; set; }
+    }
+    public class Product
+    {
+        public string ProductName { get; set; }
+    }
+}
