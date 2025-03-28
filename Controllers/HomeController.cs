11f8ae121006a1a12b975b3657278e4c802c5921
Vaishu162 <dhorawalavaishnavi@gmail.com>
2025-03-28 17:50:08 +0530

    checkout

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -26,10 +26,39 @@ namespace OnlineShopingStore.Controllers
         {
             return View();
         }
+        public ActionResult DecreaseQty(int productId)
+        {
+            if (Session["cart"] != null)
+            {
+            List<Item> cart = (List<Item>)Session["cart"];
+            var product = ctx.Tbl_Product.Find(productId);
+            foreach (var item in cart)
+            {
+                if (item.Product.ProductId == productId)
+                {
+                    int prevQty = item.Quantity;
+                        if (prevQty > 0)
+                        {
+                            cart.Remove(item);
+                            cart.Add(new Item()
+                            {
+                                Product = product,
+                                Quantity = prevQty - 1
+                            });
+                        }
+                        break;
+                    }
+                }
+                Session["cart"] = cart;
+            }
+           
+            return Redirect("CheckOut");
+        }
         public ActionResult AddToCart(int productId)
         {
             if (Session["cart"] == null)
             {
+              
                 List<Item> cart = new List<Item>();
                 var product = ctx.Tbl_Product.Find(productId);
                 cart.Add(new Item()
@@ -41,43 +70,28 @@ namespace OnlineShopingStore.Controllers
             }
             else
             {
+                
                 List<Item> cart = (List<Item>)Session["cart"];
                 var product = ctx.Tbl_Product.Find(productId);
-                foreach(var item in cart)
+
+                var existingItem = cart.FirstOrDefault(item => item.Product.ProductId == productId);
+                if (existingItem != null)
                 {
-                    if(item.Product.ProductId == productId)
-                    {
-                        int prevQty = item.Quantity;
-                        cart.Remove(item);
-                        cart.Add(new Item()
-                        {
-                            Product = product,
-                            Quantity = prevQty+1
-                        });
-                        break;
-                    }
-                    else
-                    {
-                        cart.Add(new Item()
-                        {
-                            Product = product,
-                            Quantity = 1
-                        });
-                    }
+                    existingItem.Quantity += 1;
                 }
-                
+                else
+                {
+                    cart.Add(new Item()
+                    {
+                        Product = product,
+                        Quantity = 1
+                    });
+                }
                 Session["cart"] = cart;
             }
-            //List<Item> cart = new List<Item>();
-            //var product = ctx.Tbl_Product.Find(productId);
-            //cart.Add(new Item()
-            //{
-            //    Product = product,
-            //    Quantity = 1
-            //});
-            //Session["cart"] = cart;
-            return Redirect("Index");
+            return RedirectToAction("Index");
         }
+
 
         public ActionResult RemoveFromCart(int productId)
         {
