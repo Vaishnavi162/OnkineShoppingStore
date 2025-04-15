2949baade5b15e53d6e43c851d8d7c80f985378d
Vaishu162 <dhorawalavaishnavi@gmail.com>
2025-04-15 17:52:27 +0530

    update

--- Global.asax.cs
+++ Global.asax.cs
@@ -8,6 +8,8 @@ using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
 using Stripe;
+using System.Net;
+using Razorpay;
 
 namespace OnlineShopingStore
 {
@@ -15,12 +17,14 @@ namespace OnlineShopingStore
     {
         protected void Application_Start()
         {
+            System.Net.ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
             var secretkey = WebConfigurationManager.AppSettings["StripeSecretKey"];
             StripeConfiguration.SetApiKey(secretkey);
+           
 
         }
     }
